d00c6c0da711d8c1b25d0150b045d3f3686704eb
Helio Souza <helio.souza@oi.net.br>
2025-01-13 14:16:12 -0300

    feat: implementadas as funcionalidade nas rotas

--- src/server.js
+++ src/server.js
@@ -1,20 +1,30 @@
-import http from 'node:http';
-import { routes } from './routes.js';
+import http from 'node:http'
 
-const server = http.createServer((req, res) => {
-  const { method, url } = req;
+import { json } from './midlewares/json.js'
+import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  await json(req, res)
 
   const route = routes.find(route => {
-    console.log(route.method == method && route.path == url)
-    return route.method == method && route.path == url
+    return route.method === method && route.path.test(url)
   })
 
   if (route) {
+    const routeParams = req.url.match(route.path)
+
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
 
     return route.handler(req, res)
   }
 
-  return res.end('padrão');
+  return res.writeHead(404).end()
 })
 
-server.listen(3333);
+server.listen(3333)
